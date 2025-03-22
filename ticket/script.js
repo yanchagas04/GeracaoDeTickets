851fd41006f728d0732ab8be97c18e4c08252af9
Yan Chagas <yanchagas2004@gmail.com>
2025-03-22 11:45:19 -0300

    fix: Consertando algumas coisas no ticket

--- ticket/script.js
+++ ticket/script.js
@@ -1,4 +1,9 @@
-const nome = document.getElementById('nome').textContent = localStorage.getItem('name');
-const email = document.getElementById('email').textContent = localStorage.getItem('email');
-const github = document.getElementById('github').textContent = localStorage.getItem('github');
-const avatar = document.getElementById('avatar').src = localStorage.getItem('file');
+// const nome = document.getElementById('nome').textContent = localStorage.getItem('name');
+// const email = document.getElementById('email').textContent = localStorage.getItem('email');
+// const github = document.getElementById('github').textContent = localStorage.getItem('github');
+// const avatar = document.getElementById('avatar').src = localStorage.getItem('file');
+const nome_titulo = document.getElementById('nome-titulo').textContent = localStorage.getItem('name');
+nome_titulo.textContent = localStorage.getItem('name');
+
+const email_titulo = document.getElementById('email-titulo');
+email_titulo.textContent = localStorage.getItem('email');
