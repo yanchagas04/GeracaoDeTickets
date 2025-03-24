3fd3ab3cde26fc3e0e6d01fbb8039aedc69323db
Yan Chagas <yanchagas2004@gmail.com>
2025-03-23 23:21:09 -0300

    feat: Ticket 90% funcional

--- script.js
+++ script.js
@@ -1,12 +1,68 @@
 const MAX_FILE_SIZE = 500000;
 
+function previewInput(src) {
+    const fileInput = document.getElementById('file');
+    fileInput.style.display = 'none';
+
+    const uploadIcon = document.getElementById('upload-icon');
+    uploadIcon.src = src;
+    uploadIcon.style.width = '3rem';
+    uploadIcon.style.height = '3rem';
+    uploadIcon.style.borderRadius = '0.5rem';
+    const uploadButton = document.getElementById('upload-button');
+    uploadButton.style.padding = '0';
+    const underIcon = document.getElementById('under-icon');
+    underIcon.innerHTML = '';
+
+    const changeInputButton = document.createElement('button');
+    changeInputButton.classList.add('change-input-button');
+    changeInputButton.innerHTML = 'Alterar';
+    changeInputButton.addEventListener('click', () => {
+        document.getElementById('file').click();
+    });
+    underIcon.appendChild(changeInputButton);
+
+    const removeInputButton = document.createElement('button');
+    removeInputButton.classList.add('change-input-button');
+    removeInputButton.innerHTML = 'Remover';
+    removeInputButton.addEventListener('click', () => {
+        uploadIcon.src = './assets/images/icon-upload.svg';
+        uploadButton.style.padding = '0.3rem';
+        underIcon.innerHTML = 'Clique ou arraste para enviar';
+        uploadIcon.style.width = '1.5rem';
+        uploadIcon.style.height = '1.5rem';
+        document.getElementById('file').value = '';
+        fileInput.style.display = 'block';
+        underIcon.removeChild(changeInputButton);
+        underIcon.removeChild(removeInputButton);
+    });
+    underIcon.appendChild(removeInputButton);
+}
+
 function isFileValid() {
     const fileInput = document.getElementById('file');
     const fileName = fileInput.value;
     const allowedExtensions = ['jpg', 'jpeg', 'png'];
     const fileExtension = fileName.split('.').pop().toLowerCase();
     const fileSize = fileInput.files[0].size;
-    !allowedExtensions.includes(fileExtension) ? () => {alert('Formato inválido!'), fileInput.value = ''} : fileSize > MAX_FILE_SIZE ? () => {alert('Tamanho do arquivo inválido!'), fileInput.value = ''} : null
+    
+    if (!allowedExtensions.includes(fileExtension)) {
+        alert('Por favor, envie uma imagem no formato JPG, JPEG ou PNG.');
+        fileInput.value = '';
+    } else if (fileSize > MAX_FILE_SIZE) {
+        alert('Por favor, envie uma imagem com tamanho menor ou igual a 500KB.');
+        fileInput.value = '';
+    } else {
+        let reader = new FileReader();
+
+        reader.onload = () => {
+            previewInput(reader.result)
+            
+        }
+
+        reader.readAsDataURL(fileInput.files[0]);
+        
+    }
 }
 
 function storeData() {
@@ -17,5 +73,27 @@ function storeData() {
     localStorage.setItem('name', name);
     localStorage.setItem('email', email);
     localStorage.setItem('github', github);
-    localStorage.setItem('file', file);
+
+    let reader = new FileReader();
+
+    reader.onload = () => {
+        localStorage.setItem('file', reader.result);
+    }
+
+    reader.readAsDataURL(fileInput.files[0]);
 }
+
+document.getElementById('file').addEventListener('change', function(event) {
+    const file = event.target.files[0];
+  
+    if (file) {
+      const reader = new FileReader();
+  
+      reader.onload = function(e) {
+        document.getElementById('upload-icon').src = e.target.result;
+        localStorage.setItem('file', reader.result);
+      }
+  
+      reader.readAsDataURL(file);
+    }
+  });
--- ticket/script.js
+++ ticket/script.js
@@ -1,9 +1,13 @@
-// const nome = document.getElementById('nome').textContent = localStorage.getItem('name');
-// const email = document.getElementById('email').textContent = localStorage.getItem('email');
-// const github = document.getElementById('github').textContent = localStorage.getItem('github');
-// const avatar = document.getElementById('avatar').src = localStorage.getItem('file');
-const nome_titulo = document.getElementById('nome-titulo').textContent = localStorage.getItem('name');
-nome_titulo.textContent = localStorage.getItem('name');
+let name = localStorage.getItem('name');
+const nome = document.getElementById('nome-titulo').textContent = name;
+const email = document.getElementById('email-titulo').textContent = localStorage.getItem('email');
+const github = document.getElementById('github-ticket');
+const icon = document.createElement('img');
+icon.src = '../assets/images/icon-github.svg';
+icon.width = '20';
+icon.height = '20';
+github.innerHTML = localStorage.getItem('github');
+github.appendChild(icon);
+const avatar = document.getElementById('avatar-ticket').src = localStorage.getItem('file');
 
-const email_titulo = document.getElementById('email-titulo');
-email_titulo.textContent = localStorage.getItem('email');
+const nome_ticket = document.getElementById('name-ticket').innerHTML = name;
